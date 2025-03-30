dab65ad1105b56396ea3ea7fa649d2dad62d0e8a
Pralayesh Mukherjee <pralayeshmukharjee522@gmail.com>
2025-03-31 00:02:12 +0530

    init the spring starter and add dependency like H2 database, JPA, Web, Security

---
+++ DatabaseAuthentication/src/test/java/com/Security/DatabaseAuthentication/DatabaseAuthenticationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Security.DatabaseAuthentication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DatabaseAuthenticationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
