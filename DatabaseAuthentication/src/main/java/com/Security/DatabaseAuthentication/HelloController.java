f7e8258a11f5e5c539c7ad43e840fe451c3adbbb
Pralayesh Mukherjee <pralayeshmukharjee522@gmail.com>
2025-03-31 00:14:25 +0530

    add two file for the previous folder

---
+++ DatabaseAuthentication/src/main/java/com/Security/DatabaseAuthentication/HelloController.java
@@ -0,0 +1,23 @@
+package com.Security.DatabaseAuthentication;
+
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    @GetMapping("/hello")
+    public String Hello(){
+        return "Hello Raj";
+    }
+    @GetMapping("/user")
+    @PreAuthorize("hasRole('USER')")
+    public String userLogin(){
+        return "Hello, User";
+    }
+    @GetMapping("/admin")
+    @PreAuthorize("hasRole('ADMIN')")
+    public String adminLogin(){
+        return "Hello, Admin";
+    }
+}
