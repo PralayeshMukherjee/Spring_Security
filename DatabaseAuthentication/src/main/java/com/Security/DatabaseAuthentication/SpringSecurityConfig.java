aba08d61e0f41e99434076c7e5d6950cc03f2bda
Pralayesh Mukherjee <pralayeshmukharjee522@gmail.com>
2025-03-31 15:23:17 +0530

    add the instance of datasource and autowired it

--- DatabaseAuthentication/src/main/java/com/Security/DatabaseAuthentication/SpringSecurityConfig.java
+++ DatabaseAuthentication/src/main/java/com/Security/DatabaseAuthentication/SpringSecurityConfig.java
@@ -1,5 +1,6 @@
 package com.Security.DatabaseAuthentication;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
@@ -13,13 +14,16 @@ import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
+import javax.sql.DataSource;
+
 import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
 @EnableWebSecurity
 @EnableMethodSecurity
 public class SpringSecurityConfig {
-
+    @Autowired
+    DataSource dataSource;
     @Bean
     SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
         http.authorizeHttpRequests((requests) -> requests.requestMatchers("/h2-console/**").permitAll().
