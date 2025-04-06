30925c2c718fc2370e089367ee348324a1bc1643
Pralayesh Mukherjee <pralayeshmukharjee522@gmail.com>
2025-04-06 23:26:27 +0530

    add two method for get jwt from header and key generate method

--- SpringSecurityJWT/src/main/java/com/JWT/SpringSecurityJWT/jwt/JwtUtils.java
+++ SpringSecurityJWT/src/main/java/com/JWT/SpringSecurityJWT/jwt/JwtUtils.java
@@ -1,4 +1,29 @@
 package com.JWT.SpringSecurityJWT.jwt;
 
+
+import io.jsonwebtoken.security.Keys;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import java.security.Key;
+import java.util.Base64;
+
+@Component
 public class JwtUtils {
+    @Value("${spring.app.jwtSecret}")
+    private String jwtSecret;
+    @Value("${spring.app.jwtExpiration}")
+    private int jwtExpirationMs;
+
+    public String getJwtFromHeader(HttpServletRequest request){
+        String bearerToken = request.getHeader("Authorization");
+        if(bearerToken!=null && bearerToken.startsWith("Bearer ")){
+            return bearerToken.substring(7);
+        }
+        return null;
+    }
+    private Key key(){
+        return Keys.hmacShaKeyFor(Decoder.BASE64.decode(jwtSecret));
+    }
 }
